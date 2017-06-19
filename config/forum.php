85aca29e00503562bfa9e309b66fd1ba04bac789
Deleu <deleugyn@gmail.com>
2017-06-19 06:34:58 -0300

    Add comment

--- config/forum.php
+++ config/forum.php
@@ -2,12 +2,44 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | URL Prefix
+    |--------------------------------------------------------------------------
+    |
+    | This defines the prefix for all of the packages web routes.
+    |
+    */
     'prefix'        => 'forum',
 
+    /*
+    |--------------------------------------------------------------------------
+    | Controllers Namespace
+    |--------------------------------------------------------------------------
+    |
+    | Base namespace for all controllers available in the package.
+    |
+    */
     'namespace'     => '\Bitporch\Forum\Controllers',
 
+    /*
+    |--------------------------------------------------------------------------
+    | User Model
+    |--------------------------------------------------------------------------
+    |
+    | Set your eloquent model for your users.
+    |
+    */
     'user'          => App\User::class,
 
+    /*
+    |--------------------------------------------------------------------------
+    | Middlewares
+    |--------------------------------------------------------------------------
+    |
+    | Apply middlewares for the routes registered by the package.
+    |
+    */
     'middlewares'   => [
         'web' => [
             Illuminate\Routing\Middleware\SubstituteBindings::class,
